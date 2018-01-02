47d2709dcda77e01f6185045e2a6e20ab92c4786
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-01-02 16:59:08 +0800

    蛇头 的转向跟  加速

--- Assets/Scripts/SnakeHead.cs
+++ Assets/Scripts/SnakeHead.cs
@@ -5,38 +5,77 @@ using UnityEngine;
 public class SnakeHead : MonoBehaviour
 {
     [SerializeField]
-    private int step = 1;
+    private const int baseSpeed = 1;
+    [SerializeField]
+    private const int upSpeed = 2;
+
+    private float nowSpeed = baseSpeed;
 
     private Vector2 movePos = new Vector2(1, 0);
 
     private void Update()
     {
-        CheckMove();
+        WillMove();
     }
 
-    private void CheckMove()
+    private void WillMove()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            nowSpeed = upSpeed;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            nowSpeed = baseSpeed;
+        }
+
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
 
-        if (hor != 0)
+
+        if (hor != 0 && hor * movePos.x >= 0)
         {
             movePos.x = (hor > 0 ? 1 : -1);
             movePos.y = 0;
         }
-        else if (ver != 0)
+        else if (ver != 0 && ver * movePos.y >= 0)
         {
             movePos.x = 0;
             movePos.y = (ver > 0 ? 1 : -1);
         }
+
+
         Move(movePos);
+        Rotate(movePos);
     }
 
     private void Move(Vector2 movePos)
     {
         Vector3 oldPos = transform.localPosition;
-        oldPos.x += movePos.x * Time.deltaTime * step;
-        oldPos.y += movePos.y * Time.deltaTime * step;
+        oldPos.x += movePos.x * Time.deltaTime * nowSpeed;
+        oldPos.y += movePos.y * Time.deltaTime * nowSpeed;
         transform.localPosition = oldPos;
+    }
+
+    private void Rotate(Vector2 rotate)
+    {
+        int rot = 0;
+        if (rotate.y == 1)
+        {
+            rot = 0;
+        }
+        else if (rotate.y == -1)
+        {
+            rot = 180;
+        }
+        else if (rotate.x == 1)
+        {
+            rot = 270;
+        }
+        else if (rotate.x == -1)
+        {
+            rot = 90;
+        }
+        transform.localRotation = Quaternion.Euler(0, 0, rot);
     }
 }
