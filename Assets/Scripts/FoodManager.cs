953f87a88773c39bcc9dbe175e8b35fe1f908d29
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-01-03 16:13:45 +0800

    创建食物

---
+++ Assets/Scripts/FoodManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodManager : MonoBehaviour
+{
+    /// <summary>
+    /// X 左边  Y 下边  Z 右边  W 上边
+    /// </summary>
+    [SerializeField]
+    private Vector4 border;
+    [SerializeField]
+    private SpriteRenderer foodPrefab;
+    [SerializeField]
+    private Sprite[] foodSprites;
+
+    private Transform foodRoot;
+
+    private bool canSpawn;
+
+    public FoodManager Init(Transform root)
+    {
+        if (!foodRoot)
+        {
+            foodRoot = Instantiate(new GameObject("FoodRoot"), root).transform;
+        }
+        CheckCanSpawn();
+        return this;
+    }
+
+    public bool CheckCanSpawn()
+    {
+        if (!foodRoot && !foodPrefab && foodSprites.Length <= 0)
+        {
+            canSpawn = false;
+            return canSpawn;
+        }
+        canSpawn = true;
+        return canSpawn;
+    }
+
+    public void SpawnFood()
+    {
+        if (!canSpawn) return;
+
+        SpriteRenderer newSR = Instantiate(foodPrefab, foodRoot);
+        int randomInt = Random.Range(0, foodSprites.Length);
+        newSR.sprite = foodSprites[randomInt];
+        float x, y;
+        x = Random.Range(border.x, border.z);
+        y = Random.Range(border.y, border.w);
+        newSR.transform.position = new Vector2(x, y);
+    }
+}
---
+++ Assets/Scripts/MainGameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameManager : MonoBehaviour
+{
+    public static MainGameManager Instance { get; private set; }
+    public Transform GameObjectRoot { get; private set; }
+    public FoodManager FoodManager { get; private set; }
+
+
+    private void Awake()
+    {
+        Instance = this;
+        GameObjectRoot = GameObject.Find("GameObjectRoot").transform;
+        FoodManager = GetComponent<FoodManager>().Init(GameObjectRoot);
+    }
+
+    private void Start()
+    {
+        FoodManager.SpawnFood();
+    }
+}
